4a34a324a995b519c145fd5d2d4bd911f18e3d44
Danylo Chystov <chystov@sheridancollege.ca>
2024-12-05 11:32:10 -0500

    Initial Commit

---
+++ finalExamDanyloChystov/finalExamDanyloChystovApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalExamDanyloChystovApp.swift
+//  finalExamDanyloChystov
+//
+//  Created by Dan on 2024-12-05.
+//
+
+import SwiftUI
+
+@main
+struct finalExamDanyloChystovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
